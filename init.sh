8313dc8a81bc54ae095c13cc518a741c128c2c2c
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2025-01-28 21:14:45 +0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,5 +3,8 @@
 sudo apt update -y
 sudo DEBIAN_FRONTEND="noninteractive" apt upgrade -y
 
+# apt
+curl https://raw.githubusercontent.com/chubbyhippo/wsl-ubuntu/refs/heads/main/apt.sh | /usr/bin/env sh
+
 # mise
-curl https://raw.githubusercontent.com/chubbyhippo/mise/main/install-bash.sh | /usr/bin/env bash
+curl https://raw.githubusercontent.com/chubbyhippo/mise/refs/heads/main/install-bash.sh | /usr/bin/env bash
