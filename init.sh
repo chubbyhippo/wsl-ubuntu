c49246f95ffc211cfc139ac7345d926f94e5517b
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2025-01-28 21:12:52 +0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,3 +2,6 @@
 
 sudo apt update -y
 sudo DEBIAN_FRONTEND="noninteractive" apt upgrade -y
+
+# mise
+curl https://raw.githubusercontent.com/chubbyhippo/mise/main/install-bash.sh | /usr/bin/env bash
