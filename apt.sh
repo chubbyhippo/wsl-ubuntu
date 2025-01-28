20a8877167d90021021d92daecb72676ee54601c
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2025-01-28 12:29:26 +0700

    Create apt.sh

---
+++ apt.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+sudo apt install build-essential -y
+sudo apt install unzip -y
+sudo apt install cmake -y
+sudo apt install ripgrep -y
+sudo apt install rlwrap -y
+sudo apt install fzf -y
