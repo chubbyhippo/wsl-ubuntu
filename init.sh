a56aedbbb9b636cabf9f98e9f37f25eb3f8d2372
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2025-02-01 10:07:30 +0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -13,3 +13,5 @@ curl https://raw.githubusercontent.com/chubbyhippo/fzf/refs/heads/main/install.s
 curl https://raw.githubusercontent.com/chubbyhippo/mise/refs/heads/main/install-bash.sh | /usr/bin/env bash
 # starship
 curl https://raw.githubusercontent.com/chubbyhippo/starship/main/install-bash.sh | /usr/bin/env bash
+# bashrc
+[ -f ~/.bashrc ] && curl https://raw.githubusercontent.com/chubbyhippo/wsl-ubuntu/main/.bashrc >> ~/.bashrc || curl https://raw.githubusercontent.com/chubbyhippo/wsl-ubuntu/main/.bashrc -o ~/.bashrc
