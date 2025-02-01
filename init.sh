7557000fac4b810c94232df42d0d78910190a076
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2025-02-01 08:59:12 +0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,6 +5,7 @@ sudo DEBIAN_FRONTEND="noninteractive" apt upgrade -y
 
 # apt
 curl https://raw.githubusercontent.com/chubbyhippo/wsl-ubuntu/refs/heads/main/apt.sh | /usr/bin/env sh
-
+# fzf
+curl https://raw.githubusercontent.com/chubbyhippo/wsl-ubuntu/refs/heads/main/install-fzf.sh | /usr/bin/env sh
 # mise
 curl https://raw.githubusercontent.com/chubbyhippo/mise/refs/heads/main/install-bash.sh | /usr/bin/env bash
