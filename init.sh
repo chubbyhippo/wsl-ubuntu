8534e9ae2858c27bf6abc33ed15ae91a6ac6a4f1
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2025-02-05 12:04:14 +0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -11,6 +11,8 @@ curl https://raw.githubusercontent.com/chubbyhippo/wsl-ubuntu/refs/heads/main/sn
 [ -d "${XDG_CONFIG_HOME:-$HOME/.config}/nvim" ] && rm -rf "${XDG_CONFIG_HOME:-$HOME/.config}/nvim"; mkdir -p "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim && curl https://raw.githubusercontent.com/chubbyhippo/init.lua/main/init.lua -o "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim/init.lua
 # docker 
 curl https://raw.githubusercontent.com/chubbyhippo/docker/refs/heads/main/install.sh | /usr/bin/env sh
+# minikube
+curl https://raw.githubusercontent.com/chubbyhippo/minikube/refs/heads/main/install.sh | /usr/bin/env sh
 # tilt
 curl -fsSL https://raw.githubusercontent.com/tilt-dev/tilt/master/scripts/install.sh | bash
 # fzf
