12cf6e96119ed00078b4877c6ad696c3bb87c986
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2025-02-20 09:02:47 +0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -25,3 +25,5 @@ curl https://raw.githubusercontent.com/chubbyhippo/mise/refs/heads/main/install-
 curl https://raw.githubusercontent.com/chubbyhippo/starship/main/install-bash.sh | /usr/bin/env bash
 # bashrc
 [ -f ~/.bashrc ] && curl https://raw.githubusercontent.com/chubbyhippo/wsl-ubuntu/main/.bashrc >> ~/.bashrc || curl https://raw.githubusercontent.com/chubbyhippo/wsl-ubuntu/main/.bashrc -o ~/.bashrc
+# trivy
+curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sudo sh -s -- -b /usr/local/bin v0.59.1
