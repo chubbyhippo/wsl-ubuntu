3346226e99fe5083fe6ee4e9fce629ae412571a7
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2025-03-05 19:10:05 +0700

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,39 +1,22 @@
 #!/usr/bin/env sh
 
-# Define the path to the Docker configuration directory and file
-DOCKER_CONFIG_DIR="$HOME/.docker"
-DOCKER_CONFIG_FILE="$DOCKER_CONFIG_DIR/config.json"
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-# Check if the directory exists
-if [ ! -d "$DOCKER_CONFIG_DIR" ]; then
-  echo "Directory $DOCKER_CONFIG_DIR does not exist. Creating it..."
-  mkdir -p "$DOCKER_CONFIG_DIR"
-  if [ $? -ne 0 ]; then
-    echo "Failed to create directory $DOCKER_CONFIG_DIR. Exiting..."
-    exit 1
-  fi
-else
-  echo "Directory $DOCKER_CONFIG_DIR already exists."
-fi
-
-# Create or overwrite the config.json file with the specified content
-echo "Creating $DOCKER_CONFIG_FILE with the given content..."
-(
-cat <<EOL
-{
-    "credsStore": "pass"
-}
-EOL
-) > "$DOCKER_CONFIG_FILE"
-
-# Check if the file was created successfully
-if [ $? -eq 0 ]; then
-  echo "Successfully created $DOCKER_CONFIG_FILE."
-else
-  echo "Failed to create $DOCKER_CONFIG_FILE. Exiting..."
-  exit 1
-fi
-
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
 # Verify the final content of the config.json file
 echo "The content of $DOCKER_CONFIG_FILE is:"
 cat "$DOCKER_CONFIG_FILE"
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo groupadd docker
+sudo usermod -aG docker $USER
