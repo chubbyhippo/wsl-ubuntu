00a5f5c52af14fa4bc93b34b1ed06d690b3d9199
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2025-03-20 19:20:50 +0700

    Update emacs.sh

--- emacs.sh
+++ emacs.sh
@@ -5,7 +5,5 @@ sudo snap install emacs --classic
 
 # install doomemacs
 git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs
-~/.config/emacs/bin/doom install
 echo 'export PATH="$HOME/.config/emacs/bin:$PATH"' >> ~/.bashrc
 source ~/.bashrc
-doom sync
