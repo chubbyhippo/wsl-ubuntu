0980c9310305895f20b71b5b2a245cd66a77bc78
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2025-03-20 15:49:29 +0700

    Update emacs.sh

--- emacs.sh
+++ emacs.sh
@@ -6,3 +6,4 @@ snap install emacs --classic
 # install doomemacs
 git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs
 ~/.config/emacs/bin/doom install
+echo 'export PATH="$HOME/.config/emacs/bin:$PATH"' >> ~/.bashrc
