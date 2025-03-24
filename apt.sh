f07e6a24c94c7cc802dfbfb81e582080ed138ca2
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2025-03-24 10:23:04 +0700

    Update apt.sh

--- apt.sh
+++ apt.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env sh
 
 sudo apt install build-essential -y
-sudo apt install unzip -y
 sudo apt install cmake -y
+sudo apt install fd-find -y
+sudo apt install guile-3.0 -y
+sudo apt install leiningen -y
+sudo apt install markdown -y
 sudo apt install ripgrep -y
 sudo apt install rlwrap -y
-sudo apt install leiningen -y
-sudo apt install fd-find -y
-sudo apt install markdown -y
-sudo apt install guile-3.0 -y
 sudo apt install tmux -y
+sudo apt install unzip -y
