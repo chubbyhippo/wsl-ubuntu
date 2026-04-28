52aef50d956a46e75556d27ba179a37cacdc1bd1
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2026-04-28 11:13:50 +0700

    Delete emacs.sh

--- emacs.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# install doomemacs
-git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs
-echo 'export PATH="$HOME/.config/emacs/bin:$PATH"' >> ~/.bashrc
-source ~/.bashrc
